b08af43c76e84cecf2b40feb3e8f90afa1c14810
Judson Paiva <judson.paiva@hotmail.com>
2023-07-26 00:10:04 +0100

    class Product

--- ProductDAO/product.php
+++ ProductDAO/product.php
@@ -1,3 +1,20 @@
 <?php 
 
+class Produto{
 
+    private $id;
+    private $name;
+    private $description;
+    private $price;
+    private $created_at;
+    private $update_at;
+
+    public function __construct($name, $description, $price){
+        $this->name = $name;
+        $this->description = $description;
+        $this->price = $price;
+    }
+
+    
+
+}
